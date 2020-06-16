543072655b04b19fcf4a6c88bc30c385629b8f90
Valensky etienne <valenskyetienne@MacBook-Pro.local>
2020-06-16 18:30:56 -0400

    first commit

---
+++ project1.py
@@ -0,0 +1,9 @@
+color = input("enter a color: ")
+Plural_noun = input("enter a noun: ")
+celebrity_name = input("enter a celebrity: ")
+object_name = input("enter an object: ")
+
+print("Roses are " + color )
+print(Plural_noun + " are Blue")
+print("I like " + celebrity_name)
+print("But " + object_name + " are the best")
